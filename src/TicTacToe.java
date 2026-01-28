c1012a88294b77303ecb44de05e2495b1371345d
huntingtonrobbie <hunti007@csusm.edu>
2026-01-28 11:04:26 -0800

    Added initial variables

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,4 +1,9 @@
 Public class TicTacToe {
+    static int BOARDSIZE = 3;
+    enum STATUS  {WIN, DRAW, CONTINUE};
+    char[] board;
+    bool FirstPlayer;
+    bool GameOver;
 
     public static void main() {
 
