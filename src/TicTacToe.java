50edf0dac79e48e56f7eebd5c3bb7944d950a690
huntingtonrobbie <hunti007@csusm.edu>
2026-01-28 11:13:42 -0800

    formatting & init play function

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,11 +1,13 @@
-Public class TicTacToe {
+public class TicTacToe {
     static int BOARDSIZE = 3;
-    enum STATUS  {WIN, DRAW, CONTINUE};
+    enum STATUS  {WIN, DRAW, CONTINUE}
     char[] board;
-    bool FirstPlayer;
-    bool GameOver;
+    boolean FirstPlayer;
+    boolean GameOver;
 
-    public static void main() {
+    static void play(){}
+
+     static void main() {
 
     }
 
