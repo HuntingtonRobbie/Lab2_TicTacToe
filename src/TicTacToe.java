3f5fcaceb4fce37ddfd7f2b89317e7962e15bd18
RexyRex502 <39572719+RexyRex502@users.noreply.github.com>
2026-01-28 17:49:18 -0800

    created blank tic tac toe board in main

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,13 +1,28 @@
 public class TicTacToe {
     static int BOARDSIZE = 3;
     enum STATUS  {WIN, DRAW, CONTINUE}
-    char[] board;
+    static char[] [] board;
     boolean FirstPlayer;
     boolean GameOver;
 
     static void play(){}
 
      static void main() {
+        board = new char[][] {
+                {'_', '_', '_', '_', '_', '_','_'},
+                {'|', ' ', '|', ' ', '|', ' ','|'},
+                {'|', '-', '+', '-', '+', '-','|'},
+                {'|', ' ', '|', ' ', '|', ' ','|'},
+                {'|', '-', '+', '-', '+', '-','|'},
+                {'|', ' ', '|', ' ', '|', ' ','|'},
+                {'-', '-', '-', '-', '-', '-','-'}};
+        for(char[] row : board) {
+            for(char c : row){
+                System.out.print(c);
+            }
+            System.out.println();
+        }
+
 
     }
 
